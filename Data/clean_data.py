5b00f9cebe54d06065348ff54b37d097966a7d39
Abanoub Barayo <62501003+abanoubdev@users.noreply.github.com>
2026-05-15 08:27:07 +0200

    Initialize dataset loading and display sample data in notebook

--- Data/clean_data.py
+++ Data/clean_data.py
@@ -2,9 +2,6 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 
 def get_ai_developer_dataset():   
-    """
-    This function downloads the AI Developer Performance dataset from Kaggle and returns it as a pandas DataFrame.
-    """
     file_path = kagglehub.dataset_download("ayeshasiddiqa123/ai-development")
     print("Path to dataset files:", file_path)
     df = kagglehub.dataset_load(
